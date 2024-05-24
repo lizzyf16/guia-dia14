5082934861a1242008dedf5b30acfd84b242d17e
Liz Fernandez <liz.maryfernandez@gmail.com>
2024-05-24 13:08:12 -0400

    carga de todo los datos

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="app">
+
+    <header class="py-3">
+        <h1 class="text-center">Probando API clima.</h1>
+    </header>
+
+    <main class="container">
+
+      <section class="infoClima">
+        <button class="btn btn-info" @click="actualizarClima">Actualizar clima</button>
+        <p>Última Consulta: {{ fechaConsulta }}</p>
+        <div class="py-3" v-if="datosClima.length">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Código</th>
+                <th scope="col">Estación</th>
+                <th scope="col">Hora últ. reporte</th>
+                <th scope="col">Temp.</th>
+                <th scope="col">Húmedad</th>
+                <th scope="col">Estado</th>
+                <th scope="col">Ícono</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(dato, index) in datosClima" :key="index">
+                <td>{{ dato.Codigo }}</td>
+                <td>{{dato.Estacion}}</td>
+                <td>{{dato.HoraUpdate}}</td>
+                <td>{{dato.Temp}}</td>
+                <td>{{dato.Humedad}}</td>
+                <td>{{dato.Estado}}</td>
+                <td class="fs-2">{{iconoClima(dato.Icono)}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div v-else>
+          <h2 class="text-center py-3">No hay información del clima.</h2>
+        </div>
+
+      </section>
+
+    </main>
+
+
+
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+import moment from 'moment';
+
+export default {
+  name: 'App',
+  components: {
+  
+  },
+  data(){
+    return {
+      datosClima: []
+    }
+  },
+  methods: {
+    actualizarClima: async function(){
+      try {
+
+        let response = await axios.get("https://api.gael.cloud/general/public/clima");
+
+        if(response.status == 200){
+          this.datosClima = response.data;
+          this.fechaConsulta = moment().format("DD/MM/YYYY hh:mm:ss a")
+        }else {
+          throw new Error("Problemas al obtener información del clima");
+        }    
+      } catch (error) {
+        console.log(error);
+        alert("Ha ocurrido un error al actualizar el clima.");
+      }
+    } ,
+    iconoClima: function(nombreIcono){
+      let iconosClima = {
+        "despejado.png": "☀️",
+        "parcial.png": "🌥️",
+        "cubierto.png": "☁️",
+        "pocasnubes.png" : "🌤️",
+        "niebla.png": "🌫️",
+        "llovizna.png": "🌧️",
+        "parcialalta.png": "⛅",
+        "cubiertohumo.png": "🌫️"
+      }
+      return iconosClima[nombreIcono]
+    },
+  }
+}
+</script>
+
+<style>
